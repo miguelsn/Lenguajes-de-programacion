2fd9a415e164de9b77b8d0a2957cbab0dbb71669
migueln <miguelsn243@gmail.com>
2022-08-13 11:12:24 -0700

    Tercer problema resuelto

--- Tarea-1/problemas.rkt
+++ Tarea-1/problemas.rkt
@@ -12,9 +12,14 @@
 (define (insertL x y ls)
  (if(eqv? ls '())
     null
-    (if(eqv? (first ls) 'x )
+    (if(eqv? (first ls) x )
        (cons y (cons x (insertL 'x 'y (rest ls))))
        (cons (first ls) (insertL 'x 'y (rest ls))))))
   
-
-    
+; Problema 3
+(define (remv-1st x ls)
+  (if(eqv? ls '())
+    null
+    (if(eqv? (first ls) x )
+       (rest ls)
+       (cons (first ls) (remv-1st x (rest ls))))))
