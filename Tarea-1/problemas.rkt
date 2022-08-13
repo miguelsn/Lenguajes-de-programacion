bed5d754bce8ac056f2f3f14cdfdf8f214df4f44
migueln <miguelsn243@gmail.com>
2022-08-13 11:01:48 -0700

    Segundo problema resuelto

--- Tarea-1/problemas.rkt
+++ Tarea-1/problemas.rkt
@@ -7,7 +7,14 @@
     (cons n '())
     (cons n (countdown (sub1 n)))))
 
+; Problema 2
 
+(define (insertL x y ls)
+ (if(eqv? ls '())
+    null
+    (if(eqv? (first ls) 'x )
+       (cons y (cons x (insertL 'x 'y (rest ls))))
+       (cons (first ls) (insertL 'x 'y (rest ls))))))
   
 
     
